cce85e9d4303d4e6aeeb950b1401b13faf860ff8
yihungkuo <yhkuo@atmos.ucla.edu>
2017-04-27 14:28:40 -0700

    Update bin_model_netcdf_dev.py

--- var_code/bin_model_netcdf_dev.py
+++ var_code/bin_model_netcdf_dev.py
@@ -57,35 +57,9 @@ else: # BIN_ANYWAY=True & binning_output exists
 plot_save_figure(binning_output,plot_data["args3"])
 
 # ======================================================================
-# code below adapted from MDTF example
+# code below adapted from MDTF example by Chih-Chieh (Jack) Chen, NCAR
 
-# move the plot to the proper place
-#os.system("mv "+os.environ["WKDIR"]+"/*eof1.ps "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/"+os.environ["CASENAME"]+"/.")
-#os.system("mv "+ plot_data["FIG_OUTPUT_DIR"]+plot_data["FIG_OUTPUT_FILENAME"]+ " " + os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/"+os.environ["CASENAME"]+"/.")
-
-#print 'CREATING HTML'
-# write links to html file (adapted from Jack Chen's code)
-#os.system("echo '<TABLE>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-# new row (TR)
-#os.system("echo '<TR>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-# header cells (TH)
-#os.system("echo '<TH ALIGH=LEFT><font color=navy> Convective onset diagnostics (Neelin group at UCLA)' >> "+os.environ["WKDIR"]+"/#MDTF_"+os.environ["CASENAME"]+"/variab.html")
-# header cells for each column (TH, TH)
-#os.system("echo '<TH ALIGH=LEFT>casename' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-#os.system("echo '<TH ALIGH=LEFT>OBS' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-# new row (TR)
-#os.system("echo '<TR>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-# header and links to plots in each column (TH, TH, TH)
-#os.system("echo '<TH ALIGH=LEFT>Convective Diagnostics' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-#os.system("echo '<TH ALIGH=CENTER><A HREF=\"casename/onset_diag_output_AM4-2P_tave.png\">plot</A>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-#os.system("echo '<TH ALIGH=CENTER><A HREF=\"obs/NCEP.natl.eof1.gif\">plot</A>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-#os.system("echo '<TR>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
-
-#####
-#####
-#####
-os.system("mv "+plot_data["FIG_OUTPUT_DIR"]+plot_data["FIG_OUTPUT_FILENAME"]+" "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/"+os.environ["CASENAME"]+"/.")
-
+print('Creating HTML...'),
 # remove binning outputs, if CLEAN = 1
 if os.environ["CLEAN"] == "1":
     os.system("rm -f "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/"+os.environ["CASENAME"]+"/"+data["BIN_OUTPUT_FILENAME"]+".nc")
@@ -105,3 +79,4 @@ os.system("echo '<TH ALIGH=LEFT>Tropical Ocean Basins' >> "+os.environ["WKDIR"]+
 os.system("echo '<TH ALIGH=CENTER><A HREF=\"casename/"+plot_data["FIG_OUTPUT_FILENAME"]+"\">plot</A>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
 os.system("echo '<TH ALIGH=CENTER><A HREF=\"obs/R2_TMIv7r1_200206_201405_res="+os.environ["RES"]+"_fillNrCWV_onset_diag_"+bin_data["TEMP_VAR"]+".png\">plot</A>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
 os.system("echo '<TR>' >> "+os.environ["WKDIR"]+"/MDTF_"+os.environ["CASENAME"]+"/variab.html")
+print('...Created!')
